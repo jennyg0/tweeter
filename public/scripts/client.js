7cfb76545b26bdd7e90b0f82782cc6d2456f5dc3
jennyg0 <jen2348@hotmail.com>
2020-10-07 18:16:24 +0000

    add createtweet and rendertweet functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,59 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+ const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    const $output = createTweetElement(tweet)
+    $('#tweets-container').append($output);
+  }
+ };
+
+ const createTweetElement = function(tweet) {
+  const $tweet = $(`<article>
+                    <header>
+                      <img class="resize" src="${tweet.user.avatars}">
+                      <span>${tweet.user.name}</span>
+                      <span class="name">@SirIsaac</span>
+                    </header>
+                    <div>${tweet.content.text}</div>
+                    <footer>
+                      <span>${tweet.created_at}</span>
+                      <span>
+                        <i class="fas fa-flag"></i>
+                        <i class="fas fa-retweet"></i>
+                        <i class="fas fa-heart"></i>
+                      </i></span>
+                    </footer>
+                  </article>`);
+  return $tweet;
+ }
+
+ renderTweets(data);
+});
+
