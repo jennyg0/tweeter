04d0c5ed51c6d7c094ebd6868a709d9844ad6402
jennyg0 <jen2348@hotmail.com>
2020-10-07 21:00:56 +0000

    add loadtweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -18,35 +18,12 @@ $(document).ready(function() {
   })
   });
 
-  
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
+ const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    const $output = createTweetElement(tweet)
+    $('#tweets-container').append($output);
   }
-]
-
-
-  
-
+ };
 
  const createTweetElement = function(tweet) {
   const $tweet = $(`<article>
@@ -71,16 +48,16 @@ const data = [
  const timeStamp = function(time) {
    return Date(time);
  }
+ const loadTweets = function () {
+  $.ajax({
+    url: "/tweets",
+    method: "GET"
+  }) .then ((tweets) => {
+    renderTweets(tweets);
+  })
+};
 
-
- const renderTweets = function(tweets) {
-  for (const tweet of tweets) {
-    const $output = createTweetElement(tweet)
-    $('#tweets-container').append($output);
-  }
- };
-
-
- renderTweets(data);
+ loadTweets();
+ //renderTweets(data);
 });
 
